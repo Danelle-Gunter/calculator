93ed1186500d1d92cd7c71a9816a23c05e922747
Danelle <rhowia@gmail.com>
2022-08-25 16:57:14 -0400

    Completed operate function

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,23 @@ function multiply(n1, n2) {
 }
 
 function divide(n1, n2) {
+    if (n2 === 0) return 0;
     return n1 / n2;
 }
 
+function operate(n1, n2, operand) {
+    switch (operand) {
+        case '+':
+            return add(n1, n2);
+            break;
+        case '-':
+            return subtract(n1, n2);
+            break;
+        case '*':
+            return multiply(n1, n2);
+            break;
+        case '/':
+            return divide(n1, n2);
+            break;
+    }    
+}
